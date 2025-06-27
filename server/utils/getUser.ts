6851d853a066604f55b4924a1f92b312d084d997
Fahze <ayoub.moutawakil@proton.me>
2025-06-27 15:22:40 +0200

    change - auth : inclus le profile de l'utilisateur

--- server/utils/getUser.ts
+++ server/utils/getUser.ts
@@ -3,34 +3,41 @@ export default defineEventHandler(async (event) => {
   const supabase = await useSupabase();
 
   // On récupère l'utilisateur authentifié
-  const accessToken = getHeader(event, "authorization");
+  const accessToken = getHeader(event, 'authorization');
 
   if (!accessToken) {
-    throw new Error("Authorization header is missing");
+    throw new Error('Authorization header is missing');
   }
 
   // On vérifie le token d'accès
-  if (!accessToken.startsWith("Bearer ")) {
-    throw new Error(
-      'Invalid authorization header format. Expected "Bearer <token>"'
-    );
+  if (!accessToken.startsWith('Bearer ')) {
+    throw new Error('Invalid authorization header format. Expected "Bearer <token>"');
   }
 
   // On extrait le token d'accès
-  const token = accessToken.split(" ")[1];
+  const token = accessToken.split(' ')[1];
 
   // On récupère l'utilisateur à partir du token d'accès
   if (!token) {
-    throw new Error("Access token is missing");
+    throw new Error('Access token is missing');
   }
-  
+
   const { data: userFetch, error } = await supabase.auth.getUser(token);
 
   if (!userFetch || error) {
-    throw new Error(
-      error.message || "Erreur lors de la récupération de l'utilisateur"
-    );
+    throw new Error(error.message || "Erreur lors de la récupération de l'utilisateur");
   }
 
-  return userFetch.user;
+  // On récupère le profil utilisateur
+  const { data: profile, error: profileFetchError } = await supabase.from('profile').select('*').eq('idUser', userFetch.user.id).single();
+
+  if (profileFetchError) {
+    throw new Error(profileFetchError.message || 'Erreur lors de la récupération du profil utilisateur');
+  }
+
+  return {
+    user: userFetch.user,
+    profile: profile,
+  };
+  
 });
